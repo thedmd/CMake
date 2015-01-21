f4b78cf2ac9785ae02e910ac5566718fd8592f10
Ben Boeckel <ben.boeckel@kitware.com>
2015-01-21 16:47:58 -0500

    fixup! test: test that ninja regenerates properly

--- Tests/RunCMake/ninja/check_build.cmake
+++ Tests/RunCMake/ninja/check_build.cmake
@@ -23,3 +23,19 @@ file(TIMESTAMP "${test_binary_dir}/build.ninja" rerun_time "%Y%m%d%H%M%S")
 if (NOT rerun_time GREATER orig_time)
   message(FATAL_ERROR "Ninja did not rerun? (old: ${orig_time}; new: ${rerun_time})")
 endif ()
+
+busy_loop()
+execute_process(
+  COMMAND           "${CMAKE_COMMAND}"
+                    --build .
+  OUTPUT_VARIABLE   out
+  ERROR_VARIABLE    err
+  WORKING_DIRECTORY "${test_binary_dir}")
+message("-->${out}<--")
+message("-->${err}<--")
+
+file(TIMESTAMP "${test_binary_dir}/build.ninja" noop_time "%Y%m%d%H%M%S")
+
+if (NOT rerun_time EQUAL noop_time)
+  message(FATAL_ERROR "Ninja found something to do? (old: ${rerun_time}; new: ${noop_time})")
+endif ()
