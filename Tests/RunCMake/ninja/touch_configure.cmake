ae723db5b4b4a4152547eae6166cbcc073767512
Ben Boeckel <ben.boeckel@kitware.com>
2015-01-21 16:46:43 -0500

    fixup! test: test that ninja regenerates properly

--- Tests/RunCMake/ninja/touch_configure.cmake
+++ Tests/RunCMake/ninja/touch_configure.cmake
@@ -1,5 +1,5 @@
 execute_process(
   COMMAND "${CMAKE_COMMAND}" -E touch
-          "${CMAKE_CURRENT_LIST_DIR}/test.txt.in")
+          "${test_binary_dir}/test.txt.in")
 
 include("${CMAKE_CURRENT_LIST_DIR}/check_build.cmake")
--- Tests/RunCMake/ninja/touch_try_compile.cmake
+++ Tests/RunCMake/ninja/touch_try_compile.cmake
@@ -1,5 +1,5 @@
 execute_process(
   COMMAND "${CMAKE_COMMAND}" -E touch
-          "${CMAKE_CURRENT_LIST_DIR}/tc.c")
+          "${test_binary_dir}/tc.c")
 
 include("${CMAKE_CURRENT_LIST_DIR}/check_build.cmake")
