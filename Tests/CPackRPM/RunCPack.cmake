36f93b606c4a14efdccee80b6a6a738f6795408c
Domen Vrankar <domen.vrankar@gmail.com>
2015-01-23 09:14:36 +0100

    fixup! cpack rpm multiple path relocation prefixes
    Relocatable prefix tests were failing on Fedora Linux distributions.

--- Tests/CPackRPM/RunCPack.cmake
+++ Tests/CPackRPM/RunCPack.cmake
@@ -1,13 +1,19 @@
+cmake_minimum_required(VERSION 3.1 FATAL_ERROR)
+
 file(MAKE_DIRECTORY "${dir}")
 
+# run cmake
 execute_process(
-  COMMAND ${CMAKE_CMAKE_COMMAND}
-    "${CMake_SOURCE_DIR}/Tests/CPackRPM"
+  COMMAND "${CMAKE_CMAKE_COMMAND}" "${CMake_SOURCE_DIR}/Tests/CPackRPM"
+  WORKING_DIRECTORY "${dir}"
+  OUTPUT_QUIET)
+
+# package RPM
+execute_process(
   COMMAND make package
-  RESULT_VARIABLE result
-  OUTPUT_VARIABLE stdout
+  WORKING_DIRECTORY "${dir}"
   ERROR_VARIABLE stderr
-  WORKING_DIRECTORY ${dir})
+  OUTPUT_QUIET)
 
 # test for partial relocation warning
 if(NOT "${stderr}" MATCHES "CPackRPM:Warning: Path /not_relocatable/CMakeLists.txt is not on one of the.*relocatable paths! Package will be partially relocatable.")
