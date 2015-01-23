a67be306809f9ecf87499fcb6f70f24e428d612f
Brad King <brad.king@kitware.com>
2015-01-23 09:30:55 -0500

    Revert topic 'ninja-generate-outputs'
    Byproducts like configure_file outputs cannot be listed as build rule
    outputs for Ninja or they will always be considered older than the
    CMakeCache.txt input.  Another solution will be needed.

--- Tests/RunCMake/ninja/touch_configure.cmake
+++
@@ -1,5 +0,0 @@
-execute_process(
-  COMMAND "${CMAKE_COMMAND}" -E touch
-          "${test_binary_dir}/test.txt.in")
-
-include("${CMAKE_CURRENT_LIST_DIR}/check_build.cmake")
--- Tests/RunCMake/ninja/touch_try_compile.cmake
+++
@@ -1,5 +0,0 @@
-execute_process(
-  COMMAND "${CMAKE_COMMAND}" -E touch
-          "${test_binary_dir}/tc.c")
-
-include("${CMAKE_CURRENT_LIST_DIR}/check_build.cmake")
