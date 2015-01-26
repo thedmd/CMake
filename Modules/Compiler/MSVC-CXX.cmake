ef8dcd30a0d2c3fd43105bdc598ced2115157cd4
Brad King <brad.king@kitware.com>
2015-01-26 16:42:36 -0500

    Features: Define meaning for no language standard default
    Define an empty string in CMAKE_<LANG>_STANDARD_DEFAULT to mean that
    the toolchain has no notion of lanuage standard levels.  In this case
    the <LANG>_STANDARD[_REQUIRED] properties will have no effect.

--- Modules/Compiler/MSVC-CXX.cmake
+++ Modules/Compiler/MSVC-CXX.cmake
@@ -1,9 +1,5 @@
-# No version of MSVC has full conformance to C++11. Therefore the
-# __cplusplus macro always evaluates to 98 even if the compilers come with
-# C++11/14/+ features enabled.  Later we should refactor the C++ side of
-# this within CMake to allow us to specify that there are no language level
-# flags for this compiler.
-set(CMAKE_CXX_STANDARD_DEFAULT 98)
+# MSVC has no specific language level or flags to change it.
+set(CMAKE_CXX_STANDARD_DEFAULT "")
 
 macro(cmake_record_cxx_compile_features)
   record_compiler_features(CXX "" CMAKE_CXX_COMPILE_FEATURES)
