319326525a7ecfae55fed4f3e5ce2b2dad2449be
Domen Vrankar <domen.vrankar@gmail.com>
2015-01-28 23:48:57 +0100

    fixup! cpack rpm multiple path relocation prefixes
    CPackRPM test was refactored to use RunCMake infrastructure.

--- Tests/RunCMake/CPackRPM/CPackRPM_PARTIALLY_RELOCATABLE_WARNING.cmake
+++ Tests/RunCMake/CPackRPM/CPackRPM_PARTIALLY_RELOCATABLE_WARNING.cmake
@@ -1,14 +1,7 @@
-cmake_minimum_required(VERSION 3.1 FATAL_ERROR)
-
-project(CPackRPM)
-
 install(FILES CMakeLists.txt DESTINATION /not_relocatable COMPONENT static)
 install(FILES CMakeLists.txt DESTINATION relocatable COMPONENT relocatable)
 
 set(CPACK_PACKAGE_RELOCATABLE TRUE)
-set(CPACK_GENERATOR "RPM")
 set(CPACK_PACKAGING_INSTALL_PREFIX "/opt")
 
 set(CPACK_RPM_COMPONENT_INSTALL ON)
-
-include(CPack)
